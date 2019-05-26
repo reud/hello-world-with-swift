a0491518f37c562fa7ccf01fe124258bfcd14f6c
reud <reiji-kobayashi@reud.net>
2019-05-26 20:52:40 +0900

    UIViewのテスト 11.2まで進めた

--- hello-world/ViewController.swift
+++ hello-world/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        subView.layer.cornerRadius = 16.0
+        
+        let rectView = UIView(frame: CGRect(
+            x: 20.0,
+            y: 50.0,
+            width: 30.0,
+            height: 50.0
+        ))
+        rectView.backgroundColor = .white
+        subView.addSubview(rectView)
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var subView: UIView!
+    
 }
 
