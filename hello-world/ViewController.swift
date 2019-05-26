4d8ce6657863a2d09e8cc817d4e1762e443f1787
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-05-26 20:26:40 +0900

    Initial Commit

---
+++ hello-world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello-world
+//
+//  Created by reiji kobayashi on 2019/05/26.
+//  Copyright © 2019 reud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
